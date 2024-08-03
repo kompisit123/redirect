e8216bc128189c83091d8ed9755f891e959c73da
kompisit123 <kompisit.p@gmail.com>
2024-08-03 11:19:14 +0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/:path*',
+            destination: 'https://sunev.me/:path*',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
